48e3d0a0dc932893cd1ff2f9218b33dc1220b546
ryanbrowne360 <ryanbrowne360@gmail.com>
2021-02-07 16:10:57 -0500

    changed code so that onlick didnt happen without click

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const buttonQ1 = document.getElementById("buttonq1");
 const qDiv = document.getElementById("qDiv")
 const introDiv = document.getElementById("introDiv")
 
+
 const q1Button1 = document.getElementById("q1-1")
 const q1Button2 = document.getElementById("q1-2")
 const q1Button3 = document.getElementById("q1-3")
@@ -44,26 +45,27 @@ button.onclick = function () {
     qDiv.style.display = "block";
 };
 
-q1Button1.onclick = compareAnswers(q1Button1,"Jquery",Q2,Q1);
-q1Button2.onclick = compareAnswers(q1Button2,"Jquery",Q2,Q1);
-q1Button3.onclick = compareAnswers(q1Button3,"Jquery",Q2,Q1);
-q1Button4.onclick = compareAnswers(q1Button4,"Jquery",Q2,Q1);
+q1Button1.onclick = comp1to1
+q1Button2.onclick = comp1to2
+q1Button3.onclick = comp1to3
+q1Button4.onclick = comp1to4
 
-q2Button1.onclick = compareAnswers(q1Button1,"#Example",Q3,Q2);
-q2Button2.onclick = compareAnswers(q1Button2,"#Example",Q3,Q2);
-q2Button3.onclick = compareAnswers(q1Button3,"#Example",Q3,Q2);
-q2Button4.onclick = compareAnswers(q1Button4,"#Example",Q3,Q2);
+q2Button1.onclick = comp2to1
+q2Button2.onclick = comp2to2
+q2Button3.onclick = comp2to3
+q2Button4.onclick = comp2to4
 
-q3Button1.onclick = compareAnswers(q1Button1,"var exampleVariable = 'Variable Declared'",Q4,Q3);
-q3Button2.onclick = compareAnswers(q1Button2,"var exampleVariable = 'Variable Declared'",Q4,Q3);
-q3Button3.onclick = compareAnswers(q1Button3,"var exampleVariable = 'Variable Declared'",Q4,Q3);
-q3Button4.onclick = compareAnswers(q1Button4,"var exampleVariable = 'Variable Declared'",Q4,Q3);
+q3Button1.onclick = comp3to1
+q3Button2.onclick = comp3to2
+q3Button3.onclick = comp3to3
+q3Button4.onclick = comp3to4
 
-q4Button1.onclick = compareAnswers(q1Button1,"For all of the above.",Q5,Q4);
-q4Button2.onclick = compareAnswers(q1Button2,"For all of the above.",Q5,Q4);
-q4Button3.onclick = compareAnswers(q1Button3,"For all of the above.",Q5,Q4);
-q4Button4.onclick = compareAnswers(q1Button4,"For all of the above.",Q5,Q4);
+q4Button1.onclick = comp4to1
+q4Button2.onclick = comp4to2
+q4Button3.onclick = comp4to3
+q4Button4.onclick = comp4to4
 
+//questionid.value= and do the same for button id. Easier to do if in a li?
 //maybe call a seperate function entirely for the button id's you know that have the wrong answer
 
 function compareAnswers(choice, correctChoice, nextQ, lastQ) {
@@ -77,6 +79,29 @@ function compareAnswers(choice, correctChoice, nextQ, lastQ) {
         lastQ.style.display = "none";
     }
 }
+
+//compare each question to each answer then execute the function that switches the question
+
+function comp1to1() {compareAnswers(q1Button1,"Jquery",Q2,Q1)}
+function comp1to2() {compareAnswers(q1Button2,"Jquery",Q2,Q1)}
+function comp1to3() {compareAnswers(q1Button3,"Jquery",Q2,Q1)}
+function comp1to4() {compareAnswers(q1Button4,"Jquery",Q2,Q1)}
+
+function comp2to1() {compareAnswers(q1Button1,"#Example",Q3,Q2)}
+function comp2to2() {compareAnswers(q1Button2,"#Example",Q3,Q2)}
+function comp2to3() {compareAnswers(q1Button3,"#Example",Q3,Q2)}
+function comp2to4() {compareAnswers(q1Button4,"#Example",Q3,Q2)}
+
+function comp3to1() {compareAnswers(q1Button1,"var exampleVariable = 'Variable Declared'",Q4,Q3)}
+function comp3to2() {compareAnswers(q1Button2,"var exampleVariable = 'Variable Declared'",Q4,Q3)}
+function comp3to3() {compareAnswers(q1Button3,"var exampleVariable = 'Variable Declared'",Q4,Q3)}
+function comp3to4() {compareAnswers(q1Button4,"var exampleVariable = 'Variable Declared'",Q4,Q3)}
+
+function comp4to1() {compareAnswers(q1Button1,"For all of the above.",Q5,Q4)}
+function comp4to2() {compareAnswers(q1Button2,"For all of the above.",Q5,Q4)}
+function comp4to3() {compareAnswers(q1Button3,"For all of the above.",Q5,Q4)}
+function comp4to4() {compareAnswers(q1Button4,"For all of the above.",Q5,Q4)}
+
 
 
 
